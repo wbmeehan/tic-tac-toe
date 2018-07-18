59d785dc1788d4e6413b442b794e31406491c5c4
William Meehan <william.meehan@uqconnect.edu.au>
2018-07-18 14:19:50 +1000

    implemented play against a friend mode

--- TicTacToe/Models/MainWindowModel.cs
+++ TicTacToe/Models/MainWindowModel.cs
@@ -1,9 +1,227 @@
-﻿using System.Text;
-using MvvmFoundation.Wpf;
+﻿using MvvmFoundation.Wpf;
+using System;
+using System.Collections.Generic;
 
 namespace TicTacToe.Models
 {
+    /// <summary>
+    /// Tile states
+    /// </summary>
+    public enum TileState { Default, X, O };
     class MainWindowModel : ObservableObject
     {
+        #region Fields
+
+        /// <summary>
+        /// Game grid dimensions
+        /// </summary>
+        private const int GridSize = 3;
+
+        /// <summary>
+        /// Game grid
+        /// </summary>
+        private TileState[,] _grid = new TileState[GridSize, GridSize];
+
+        /// <summary>
+        /// Move count
+        /// </summary>
+        private int _moveCount;
+
+        private bool _isGameOver;
+
+
+        #endregion
+
+        #region Properties
+
+        public Queue<string> WinningTiles { get; set; } = new Queue<string>();
+
+        public TileState MoveLetter
+        {
+            get
+            {
+                if (_moveCount % 2 == 0)
+                {
+                    return TileState.O;
+                } else
+                {
+                    return TileState.X;
+                }
+            }
+        }
+
+        public bool IsGameOver {
+            get
+            {
+                return _isGameOver;
+            }
+        }
+
+        /// <summary>
+        /// Game grid dimensions
+        /// </summary>
+        public TileState[,] Grid
+        {
+            get
+            {
+                return _grid;
+            }
+
+            set
+            {
+                Grid = value;
+            }
+        }
+
+        #endregion
+
+        public bool MakeMove(string tilePosition)
+        {
+
+            int row = tilePosition[0] - '0';
+            int column = tilePosition[1] - '0';
+
+            if (IsGameOver)
+            {
+                ResetGrid();
+            }
+
+            if (Grid[row, column] != TileState.Default)
+            {
+                /* Invalid move */
+                return false;
+            }
+
+            _moveCount++;
+            Grid[row, column] = MoveLetter;
+
+            if (IsWinner(row, column) || IsDraw())
+            {
+                _isGameOver = true;
+            }
+
+            return true;
+        }
+
+        public void ResetGrid()
+        {
+            _moveCount = 0;
+            Array.Clear(Grid, 0, Grid.Length);
+            _isGameOver = false;
+        }
+
+        private bool IsWinner(int row, int column)
+        {
+            
+            if (IsColumnWin(column))
+            {
+                for (int i = 0; i < GridSize; i++)
+                {
+                    WinningTiles.Enqueue(String.Format("{0}{1}", i, column));
+                }
+                return true;
+            }
+
+            if (IsRowWin(row))
+            {
+                for (int i = 0; i < GridSize; i++)
+                {
+                    WinningTiles.Enqueue(String.Format("{0}{1}", row, i));
+                }
+                return true;
+            }
+
+            if (row == column && IsMainDiagonalWin())
+            {
+                for (int i = 0; i < GridSize; i++)
+                {
+                    WinningTiles.Enqueue(String.Format("{0}{1}", i, i));
+                }
+                return true;
+            }
+
+            if (((row + column) == GridSize - 1) &&
+                IsAntiDiagonalWin())
+            {
+                for (int i = 0; i < GridSize; i++)
+                {
+                    WinningTiles.Enqueue(String.Format("{0}{1}", i,
+                        GridSize - 1 - i));
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsColumnWin(int column)
+        {
+            for (int i = 0; i < GridSize; i++) {
+                if (_grid[i, column] != MoveLetter)
+                {
+                    break;
+                }
+                if (i == GridSize - 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsRowWin(int row)
+        {
+            for (int i = 0; i < GridSize; i++)
+            {
+                if (_grid[row, i] != MoveLetter)
+                {
+                    break;
+                }
+                if (i == GridSize - 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsMainDiagonalWin()
+        {
+            for (int i = 0; i < GridSize; i++)
+            {
+                if (Grid[i, i] != MoveLetter)
+                {
+                    break;
+                }
+                if (i == GridSize - 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsAntiDiagonalWin()
+        {
+            for (int i = 0; i < GridSize; i++)
+            {
+                if (Grid[GridSize - 1 - i, i] != MoveLetter)
+                {
+                    break;
+                }
+                if (i == GridSize - 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool IsDraw()
+        {
+            return _moveCount == Math.Pow(GridSize, 2);
+        }
+
     }
 }
--- TicTacToe/ViewModels/MainWindowViewModel.cs
+++ TicTacToe/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Text;
+﻿using MvvmFoundation.Wpf;
+using System.Collections.Generic;
 using System.Windows.Input;
 using TicTacToe.Models;
-using MvvmFoundation.Wpf;
 
 namespace TicTacToe.ViewModels
 {
@@ -13,6 +12,197 @@ namespace TicTacToe.ViewModels
         /* Tic-tac-toe model */
         private MainWindowModel _ticTacToeModel = new MainWindowModel();
 
+        /* Click command */
+        private ICommand _tileClickCommand;
+
         #endregion
+
+        #region Public Properties/Commands
+
+        /* Tile labels */
+        public string Tile00Label { get; set; }
+        public string Tile01Label { get; set; }
+        public string Tile02Label { get; set; }
+        public string Tile10Label { get; set; }
+        public string Tile11Label { get; set; }
+        public string Tile12Label { get; set; }
+        public string Tile20Label { get; set; }
+        public string Tile21Label { get; set; }
+        public string Tile22Label { get; set; }
+
+        /* Tile label colours */
+        public string Tile00Colour { get; set; } = "Yellow";
+        public string Tile01Colour { get; set; } = "Yellow";
+        public string Tile02Colour { get; set; } = "Yellow";
+        public string Tile10Colour { get; set; } = "Yellow";
+        public string Tile11Colour { get; set; } = "Yellow";
+        public string Tile12Colour { get; set; } = "Yellow";
+        public string Tile20Colour { get; set; } = "Yellow";
+        public string Tile21Colour { get; set; } = "Yellow";
+        public string Tile22Colour { get; set; } = "Yellow";
+
+        #region Tic-tac-toe model variables
+
+        public TileState[,] Grid
+        {
+            get
+            {
+                return _ticTacToeModel.Grid;
+            }
+        }
+
+        public TileState MoveLetter
+        {
+            get
+            {
+                return _ticTacToeModel.MoveLetter;
+            }
+        }
+
+        public Queue<string> WinningTiles
+        {
+            get
+            {
+                return _ticTacToeModel.WinningTiles;
+            }
+
+            set
+            {
+
+            }
+        }
+
+        public bool IsGameOver
+        {
+            get
+            {
+                return _ticTacToeModel.IsGameOver;
+            }
+        }
+
+
+        #endregion
+
+        public ICommand TileClickCommand
+        {
+            get
+            {
+                if (_tileClickCommand == null)
+                {
+                    _tileClickCommand = new RelayCommand<string>(TileClick);
+
+                }
+                return _tileClickCommand;
+            }
+
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        /// <summary>
+        /// Register move on tile.
+        /// </summary>
+        private void TileClick(string tilePosition)
+        {
+
+            if (IsGameOver)
+            {
+                resetTileTextColour();
+            }
+
+            if (_ticTacToeModel.MakeMove(tilePosition) == false)
+            {
+                /* Invalid move */
+                return;
+            }
+
+            updateTileText();
+
+            if (IsGameOver)
+            {
+                while (WinningTiles.Count != 0)
+                {
+                    setTileTextColour(WinningTiles.Dequeue(), "LimeGreen");
+                }
+            } 
+            
+        }
+
+        private void updateTileText()
+        {
+            Tile00Label = (Grid[0, 0] != TileState.Default) ? Grid[0, 0].ToString() : "";
+            Tile01Label = (Grid[0, 1] != TileState.Default) ? Grid[0, 1].ToString() : "";
+            Tile02Label = (Grid[0, 2] != TileState.Default) ? Grid[0, 2].ToString() : "";
+            Tile10Label = (Grid[1, 0] != TileState.Default) ? Grid[1, 0].ToString() : "";
+            Tile11Label = (Grid[1, 1] != TileState.Default) ? Grid[1, 1].ToString() : "";
+            Tile12Label = (Grid[1, 2] != TileState.Default) ? Grid[1, 2].ToString() : "";
+            Tile20Label = (Grid[2, 0] != TileState.Default) ? Grid[2, 0].ToString() : "";
+            Tile21Label = (Grid[2, 1] != TileState.Default) ? Grid[2, 1].ToString() : "";
+            Tile22Label = (Grid[2, 2] != TileState.Default) ? Grid[2, 2].ToString() : "";
+        }
+
+        private void resetTileTextColour()
+        {
+            Tile00Colour = "Yellow";
+            Tile01Colour = "Yellow";
+            Tile02Colour = "Yellow";
+            Tile10Colour = "Yellow";
+            Tile11Colour = "Yellow";
+            Tile12Colour = "Yellow";
+            Tile20Colour = "Yellow";
+            Tile21Colour = "Yellow";
+            Tile22Colour = "Yellow";
+        }
+
+        private void setTileTextColour(string tilePosition, string tileColour)
+        {
+            switch (tilePosition)
+            {
+                case "00":
+
+                    Tile00Colour = tileColour;
+                    break;
+                case "01":
+
+                    Tile01Colour = tileColour;
+                    break;
+                case "02":
+
+                    Tile02Colour = tileColour;
+                    break;
+                case "10":
+
+                    Tile10Colour = tileColour;
+                    break;
+                case "11":
+
+                    Tile11Colour = tileColour;
+                    break;
+                case "12":
+
+                    Tile12Colour = tileColour;
+                    break;
+                case "20":
+
+                    Tile20Colour = tileColour;
+                    break;
+                case "21":
+
+                    Tile21Colour = tileColour;
+                    break;
+                case "22":
+
+                    Tile22Colour = tileColour;
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+        #endregion
+
     }
 }
